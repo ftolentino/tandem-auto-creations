b0179f62085041e67348437df375c2e38ff265b1
Filmer Tolentino <filmer.tolentino@gmail.com>
2020-02-07 14:32:31 -0800

    added build folder

---
+++ build/precache-manifest.4a7dc043bab8f532e475e5847cdcd880.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "164692dae7aadc57a36a2ad8c231fdfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed9bdfc144b9bdbc9a85",
+    "url": "/static/css/main.9c90668f.chunk.css"
+  },
+  {
+    "revision": "fd30ae123f5c92e516bb",
+    "url": "/static/js/2.ca904903.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ca904903.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed9bdfc144b9bdbc9a85",
+    "url": "/static/js/main.2771951d.chunk.js"
+  },
+  {
+    "revision": "956e6ff467fc045364b5",
+    "url": "/static/js/runtime-main.b6bb45d3.js"
+  }
+]);
